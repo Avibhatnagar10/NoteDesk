b53770c0cf35f59545faf5f21ffb8c9ea6ee139c
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2025-08-31 01:37:06 +0530

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,22 +3,21 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+  email: { type: String, required: true, unique: true },
+  passwordHash: { type: String, required: true },
+}, { timestamps: true });
 
-// Hash password before save
-userSchema.pre("save", async function(next) {
+// Encrypt password before saving
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 // Compare password
-userSchema.methods.comparePassword = function(password) {
-  return bcrypt.compare(password, this.password);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-export default mongoose.model("User", userSchema);
-
-
+const User = mongoose.model("User", userSchema);
+export default User;
