393871d77ddd2c5ba24b4877283fc2ffa0fd6c92
Avi Bhatnagar <91671078+Avibhatnagar10@users.noreply.github.com>
2025-09-01 12:24:31 +0530

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,22 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 import NotesEditor from "./pages/notesEditor";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/settings" element={<Settings />} />
         <Route path="/notesEditor" element={<NotesEditor />} />
       </Routes>
